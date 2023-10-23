41b9d0fd6775541dcf7e191dc447bff3fb8bab61
Dominik Josipovic <dominik.josipovic@gmail.com>
2023-10-23 23:53:40 +0200

    Dodana PrepaidKartica

---
+++ PrepaidKartica/PrepaidForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STATE_PrepaidKartica
+{
+    public partial class PrepaidForm : Form
+    {
+        private PrepaidKartica _kartica;
+        public PrepaidForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PrepaidForm_Load(object sender, EventArgs e)
+        {
+            _kartica = new PrepaidKartica("HR12312414213");
+            txtSerijskiBroj.Text = _kartica.SerijskiBroj;
+            txtStatus.Text = "Nije aktivna";
+        }
+
+        private void btnAktiviraj_Click(object sender, EventArgs e)
+        {
+            _kartica.Aktiviraj();
+            btnAktiviraj.Enabled = false;
+            txtStatus.Text = "Aktivna";
+            txtIznos.Text = _kartica.Iznos.ToString();
+        }
+
+        private void btnUplati_Click(object sender, EventArgs e)
+        {
+            double iznosUplate = double.Parse(txtIznosUplate.Text);
+            _kartica.Uplati(iznosUplate);
+            txtIznosUplate.Clear();
+            if (btnAktiviraj.Enabled == false)
+            {
+                txtIznos.Text = _kartica.Iznos.ToString();
+                txtStatus.Text = "Aktivna";
+            }
+        }
+
+        private void btnIsplati_Click(object sender, EventArgs e)
+        {
+            if (btnAktiviraj.Enabled == true)
+            {
+                txtIznosIsplate.Clear();
+                return;
+            }
+
+            double iznosIsplate = double.Parse(txtIznosIsplate.Text);
+            if (_kartica.Iznos >= iznosIsplate)
+            {
+                _kartica.Isplati(iznosIsplate);
+                txtIznos.Text = _kartica.Iznos.ToString();
+            }
+            else
+            {
+                txtStatus.Text = "Nema sredstava";
+            }
+            txtIznosIsplate.Clear();
+        }
+    }
+}
---
+++ PrepaidKartica/PrepaidKartica.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STATE_PrepaidKartica
+{
+    public class PrepaidKartica
+    {
+        public string SerijskiBroj { get; set; }
+        public double Iznos { get; set; }
+
+        public PrepaidKartica(string serijskiBroj)
+        {
+            SerijskiBroj = serijskiBroj;
+        }
+
+        public void Aktiviraj()
+        {
+            Iznos = 100;
+        }
+
+        public void Uplati(double iznosUplate)
+        {
+            Iznos += iznosUplate;
+        }
+
+        internal void Isplati(double iznosIsplate)
+        {
+            Iznos -= iznosIsplate;
+        }
+    }
+}
